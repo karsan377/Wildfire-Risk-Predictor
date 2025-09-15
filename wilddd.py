9526c5cf18a27da800205da0356f6858474aa843
Karthik Sankaran <karthik@MacBookPro.lan>
2025-09-15 15:51:59 -0700

    changed port

--- wilddd.py
+++ wilddd.py
@@ -175,4 +175,5 @@ def verify_city():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's port if available, else 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
